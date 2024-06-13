d36f243006c5c41b99e61da9a2deca7e7698d80d
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-06-12 23:21:59 -0400

    Add sign in redirection

--- background.js
+++ background.js
@@ -1,15 +1,6 @@
 chrome.webNavigation.onBeforeNavigate.addListener(details => {
-  zoomPattern = /^https:\/\/\w+\.\w+\.\w+\/j\/\d{10}\?pwd\=[a-zA-Z0-9]{32}$/;
-
-  if (details.url.match(zoomPattern)) {
+  zoomPattern = /^https:\/\/(\w+\.)?\w+\.\w+\/j\/\d{10}\?pwd\=[a-zA-Z0-9]{32}$/;
+  if (details.url.match(zoomPattern))
     chrome.tabs.update(details.tabId, {url: `${details.url}#success`});
+}, {url: [{urlMatches: ".*"}]});;
 
-    const selector = '#zoom-ui-frame > div.bhauZU7H > div > div.pUmU_FLW > h3:nth-child(2) > span > a';
-    const element = document.querySelector(selector);
-    if (element) {
-      element.click();
-    } else {
-      console.error('Element not found');
-    }
-  }
-}, {url: [{urlMatches: ".*"}]});; 
