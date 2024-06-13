78ff0e127a2d388450edb58439e11e1a19caa592
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-06-12 21:30:11 -0400

    V1

--- background.js
+++ background.js
@@ -1,10 +1,15 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    const url = new URL(details.url);
-    if (!url.hash) { // Only redirect if no hash is present                                                                                                                                                      
-      return {redirectUrl: details.url + "#success"};
+chrome.webNavigation.onBeforeNavigate.addListener(details => {
+  zoomPattern = /^https:\/\/\w+\.\w+\.\w+\/j\/\d{10}\?pwd\=[a-zA-Z0-9]{32}$/;
+
+  if (details.url.match(zoomPattern)) {
+    chrome.tabs.update(details.tabId, {url: `${details.url}#success`});
+
+    const selector = '#zoom-ui-frame > div.bhauZU7H > div > div.pUmU_FLW > h3:nth-child(2) > span > a';
+    const element = document.querySelector(selector);
+    if (element) {
+      element.click();
+    } else {
+      console.error('Element not found');
     }
-  },
-  {urls: ["*://*.zoom.us/j/*"]}, // Restricts the listener to Zoom meeting links                                                                                                                                 
-  ["blocking"]
-); 
+  }
+}, {url: [{urlMatches: ".*"}]});; 
